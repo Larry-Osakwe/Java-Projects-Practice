b4beb2681890a6d392003cee7367fac3dc725ca0
Larry <larryosak@gmail.com>
2021-07-15 20:37:53 -0700

    add outline instructions and initial variables

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,6 +4,14 @@ public class EmailApp {
 
     public static void main(String[] args) {
 
+        //todo
+        // Generate an email with the following syntax: firstname.lastname@department.company.com
+        // Determine the department (sales, development, accounting), if none leave blank
+        // Generate a random String for a password
+        // Have set methods to change the password, set the mailbox capacity, and define an alternate email address
+        // Have get methods to display the name, email, and mailbox
+
+
     }
 
 }
