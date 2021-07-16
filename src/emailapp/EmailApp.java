d7869507079245ddff78cd75f4d616c5e5b031e0
Larry <larryosak@gmail.com>
2021-07-15 20:44:07 -0700

    create email constructor

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -6,6 +6,8 @@ public class EmailApp {
 
         //todo
         // Generate an email with the following syntax: firstname.lastname@department.company.com
+            Email em1 = new Email("Larry", "Osakwe");
+
         // Determine the department (sales, development, accounting), if none leave blank
         // Generate a random String for a password
         // Have set methods to change the password, set the mailbox capacity, and define an alternate email address
