698f057e1fb80c0b4f0885518a7cc622530ab7d5
Larry <larryosak@gmail.com>
2021-07-19 16:58:53 -0700

    ask user for department code

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -12,9 +14,28 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.println("EMAIL CREATED FOR " + this.firstName + " " + this.lastName );
+
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
 
     // Ask for the department
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter Department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) {
+            return "sales";
+        } else if (depChoice == 2) {
+            return "dev";
+        } else if (depChoice == 3) {
+            return "acct";
+        } else {
+            return "";
+        }
+    }
+
 
     // Generate random password
 
