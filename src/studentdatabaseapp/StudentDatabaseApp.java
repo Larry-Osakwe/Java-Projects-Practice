b0c9ca00d7b9edf839199897d4cd30a4963c39e1
Larry <larryosak@gmail.com>
2021-07-19 20:04:25 -0700

    add outline and initial variables

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,7 +3,9 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
+        // Ask how many new users we want to add
 
+        // Create n number of new students
     }
 
 }
