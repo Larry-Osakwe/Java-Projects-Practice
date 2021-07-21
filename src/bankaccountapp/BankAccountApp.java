478afcc01c7cb2374b8fe89749b9f11382306aae
Larry <larryosak@gmail.com>
2021-07-21 12:50:16 -0700

    add CSV reader utility

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,20 +1,25 @@
 package bankaccountapp;
 
+import java.util.List;
+
 public class BankAccountApp {
 
     public static void main(String[] args) {
 
-        Checking chkAcc1 = new Checking("John Smith", "123432134", 50);
-        Savings savAcc1 = new Savings("John Smith", "123432134", 50);
+        String file = "D:\\Libraries\\Documents\\Java Learning\\JProjs\\src\\NewBankAccounts.csv";
 
-
-        savAcc1.compound();
-
-        chkAcc1.showInfo();
-        System.out.println("***************");
-        savAcc1.showInfo();
+//        Checking chkAcc1 = new Checking("John Smith", "123432134", 50);
+//        Savings savAcc1 = new Savings("John Smith", "123432134", 50);
+//
+//
+//        savAcc1.compound();
+//
+//        chkAcc1.showInfo();
+//        System.out.println("***************");
+//        savAcc1.showInfo();
 
         // Read a CSV file and create new accounts based on that data
+        List<String[]> newAccountHolder = utilities.CSV.read(file);
     }
 
 }
