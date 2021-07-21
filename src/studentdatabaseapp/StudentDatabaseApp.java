745a22d273de53c5c48f1b2e1cd58fc06fdb0389
Larry <larryosak@gmail.com>
2021-07-20 17:15:39 -0700

    prompt user for number of students and create students based on that number

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,13 +1,24 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
+
         // Ask how many new users we want to add
-        Student stu = new Student();
-        stu.enroll();
-        stu.payTuition(400);
+        System.out.print("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
         // Create n number of new students
+        for (int i = 0; i < numOfStudents; i++) {
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+        }
+
     }
 
 }
