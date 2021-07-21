f1ac990ef64ff4aa156575ddeeed84abbafbf388
Larry <larryosak@gmail.com>
2021-07-21 13:02:44 -0700

    read data from file and determine it's being parsed correctly

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -20,6 +20,26 @@ public class BankAccountApp {
 
         // Read a CSV file and create new accounts based on that data
         List<String[]> newAccountHolder = utilities.CSV.read(file);
+
+        for (String[] accountHolder : newAccountHolder) {
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+
+//            System.out.println(name + " " + sSN + " " + accountType + " " + initDeposit);
+
+            if (accountType.equals("Checking")) {
+                System.out.println("Check check");
+            } else if (accountType.equals("Savings")) {
+                System.out.println("Save Save");
+            } else {
+                System.out.println("Error");
+            }
+
+        }
+
+
     }
 
 }
